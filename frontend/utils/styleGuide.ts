5901924293bc742f51e42d32b22129cca6ace236
ddiaalv <ddiaalv@gmail.com>
2021-07-11 11:36:52 +0100

    :art: New colors

--- frontend/utils/styleGuide.ts
+++ frontend/utils/styleGuide.ts
@@ -4,18 +4,22 @@ export const styleGuide = {
     pink: {
       light: '#fa598e',
       dark: '#cf3a6b',
+      gradient: `linear-gradient(130deg, #fa598e, #cf3a6b);`,
     },
     yellow: {
       light: '#ffeb7a',
       dark: '#f3c450',
+      gradient: `linear-gradient(130deg, #ffeb7a, #f3c450);`,
     },
     blue: {
       light: '#59ccfa',
       dark: '#389dc6',
+      gradient: `linear-gradient(130deg, #59ccfa, #389dc6);`,
     },
     green: {
       light: '#59fa89',
       dark: '#35c35f',
+      gradient: `linear-gradient(130deg, #59fa89, #35c35f);`,
     },
   },
   device: {
